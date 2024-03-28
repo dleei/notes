75e4bcf65b2825b21394b16efb7e968fe89f6ff6
dl <dailei.mail@qq.com>
2024-03-28 11:56:27 +0800

    wallpaper项目初始化

---
+++ 小程序/uni_app/uni_app/wallpaper/pages/index/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <view class="caontainer">
+    <h1>第一个uni_app项目</h1>
+  </view>
+</template>
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  h1 {
+    font-size: 25rpx;
+    color: blue;
+  }
+}
+</style>
+<script setup></script>
