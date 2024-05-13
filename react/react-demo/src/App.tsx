98bc772164e8bc31074b218a09002ee236e43a97
dl <dailei.mail@qq.com>
2024-05-13 23:56:57 +0800

    feat：🎉context上下文深入传递数据

--- react/react-demo/src/App.tsx
+++ react/react-demo/src/App.tsx
@@ -1,31 +1,46 @@
-import { useState, useMemo } from 'react'
+import { createContext, useContext } from 'react'
+//  1. 使用 createContext 创建一个上下文对象
+//  2. 定义一个 Provider 组件，用于提供数据,在顶层组件使用
+//  3. 在需要使用的地方通过 useContext 获取上下文中提供的对象，在需要使用数据的地方使用
+const Context = createContext('')
 
 const App = () => {
-  const [count, setCount] = useState(1)
-  /*   这里，useMemo 接收两个参数：
-创建函数：这是一个函数，用于计算记忆值。这个函数会在初次渲染时执行，并在其依赖项改变时再次执行。
-依赖数组：这是一个数组，其中包含了从外部作用域中获取的所有值，这些值决定了什么时候需要重新计算记忆值。如果这些值自上次渲染以来未发生变化，则 useMemo 将复用上次的记忆值而不是重新计算。 */
-  const calculations = useMemo(() => {
-    const doubleCount = count * 2
-    const tripleCount = count * 3
-    return {
-      doubleCount,
-      tripleCount,
-    }
-  }, [count])
-  const handleClick = () => {
-    setCount(count + 1) //
-  }
-
+  const msg = '父组件的数据'
   return (
     <div>
-      doubleCount: {calculations.doubleCount}
-      <br />
-      tripleCount:{calculations.tripleCount}
-      <br />
-      <button onClick={handleClick}>计算属性</button>
+      <Context.Provider value={ msg }>
+        父组件
+        <Son />
+      </Context.Provider>
     </div>
   )
 }
 
+const Son = () => {
+  return (
+    <div>
+      子组件
+      <Grandson />
+    </div>
+  )
+}
+
+const Grandson = () => {
+  return (
+    <div>
+      孙子组件
+      <Lower />
+    </div>
+  )
+}
+
+const Lower = () => {
+  const value = useContext(Context)  // 使用 useContext 获取上下文中的数据
+  return (
+    <div>
+      底层组件
+      {value}
+    </div>
+  )
+}
 export default App
--- react/笔记/react入门.md
+++ react/笔记/react入门.md
@@ -994,9 +994,57 @@ const Child = ({ header,body }) => {
 export default App
 ```
 
+### 复杂组件之间的数据通讯
 
+```tsx
+import { createContext, useContext } from 'react'
+//  1. 使用 createContext 创建一个上下文对象
+//  2. 定义一个 Provider 组件，用于提供数据,在顶层组件使用
+//  3. 在需要使用的地方通过 useContext 获取上下文中提供的对象，在需要使用数据的地方使用
+const Context = createContext('')
 
+const App = () => {
+  const msg = '父组件的数据'
+  return (
+    <div>
+      <Context.Provider value={ msg }>
+        父组件
+        <Son />
+      </Context.Provider>
+    </div>
+  )
+}
 
+const Son = () => {
+  return (
+    <div>
+      子组件
+      <Grandson />
+    </div>
+  )
+}
+
+const Grandson = () => {
+  return (
+    <div>
+      孙子组件
+      <Lower />
+    </div>
+  )
+}
+
+const Lower = () => {
+  const value = useContext(Context)  // 使用 useContext 获取上下文中的数据
+  return (
+    <div>
+      底层组件
+      {value}
+    </div>
+  )
+}
+export default App
+
+```
 
 
 
