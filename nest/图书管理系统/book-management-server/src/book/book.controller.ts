fdc3b6c9c862c43c1465ee3f33273e8021537310
DL <dailei.mail@qq.com>
2024-11-22 10:21:17 +0800

    feat:🎉nest图书管理系统练习后端接口及前端框架

---
+++ nest/图书管理系统/book-management-server/src/book/book.controller.ts
@@ -0,0 +1,74 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Param,
+  Delete,
+  UseInterceptors,
+  UploadedFile,
+  BadRequestException,
+} from '@nestjs/common';
+import { BookService } from './book.service';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { storage } from './my-file-storage';
+
+import * as path from 'path';
+
+@Controller('book')
+export class BookController {
+  constructor(private readonly bookService: BookService) {}
+
+  @Post('create')
+  create(@Body() createBookDto: CreateBookDto) {
+    return this.bookService.create(createBookDto);
+  }
+
+  @Get('list')
+  async list() {
+    return this.bookService.list();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.bookService.findById(+id);
+  }
+
+  @Put('update')
+  async update(@Body() updateBookDto: UpdateBookDto) {
+    return this.bookService.update(updateBookDto);
+  }
+
+  @Delete('delete/:id')
+  async delete(@Param('id') id: string) {
+    return this.bookService.delete(+id);
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'upload',
+      storage: storage,
+      limits: {
+        fileSize: 1024 * 1024 * 5, // 5MB
+      },
+      fileFilter: (req, file, cb) => {
+        const extName = path.extname(file.originalname);
+        const allowedExt = ['.jpg', '.jpeg', '.png', '.gif'];
+        if (allowedExt.includes(extName)) {
+          cb(null, true);
+        } else {
+          cb(new BadRequestException('Invalid file type'), false);
+        }
+      },
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file', file);
+
+    return file.path;
+  }
+}
---
+++ nest/图书管理系统/book-management-server/src/user/user.controller.ts
@@ -0,0 +1,31 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { UserService } from './user.service';
+import { RegisterUserDto } from './dto/register-user-dto';
+import { LoginUserDto } from './dto/login-user-dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  /**
+   * @description  用户注册
+   * @params registerDto 用户注册信息
+   * @returns  
+   */
+
+  @Post('register')
+  register(@Body() registerDto: RegisterUserDto) {
+    return this.userService.register(registerDto);
+  }
+  
+  /**
+   * @description  用户登录
+   * @param loginUserDto 用户登录
+   * @returns 
+   */
+  
+  @Post('login')
+  login(@Body()loginUserDto: LoginUserDto) {
+    return this.userService.login(loginUserDto);
+  }
+}
